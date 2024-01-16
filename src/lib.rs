22861c47ebb3112913888ca08df78d3873ce46c3
admercs <adam.michael.erickson@gmail.com>
2024-01-15 23:07:38 -0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
-
 /// Recursive ternary operation for terse if-else statements.
 ///
 /// # Uses
-/// 
+///
 /// Provides brief if-else statement syntax similar to other languages.
 ///
 /// Reducing the lines-of-code (LOC) of chained if-else conditional
@@ -10,7 +9,7 @@
 ///
 /// Other use-cases of `ifelse!` include providing natural recursion of chained
 /// conditional branches.
-/// 
+///
 /// If only a value is entered, that value is returned. This is necessary to
 /// capture false statement remainders.
 ///
@@ -19,21 +18,22 @@
 /// ```
 /// // A single if-else statement.
 /// ifelse!(1 < 0, true, false);
-/// 
+///
 /// // Chained if-else statements.
 /// ifelse!(1 < 0, true, 1 > 0, true, false);
 /// ifelse!(1 < 0, true, 0 > 1, true, false);
 /// ifelse!(1 < 0, true, 0 > 1, false, 0 != 0, 1, 0);
-/// 
+///
 /// // A single input returns itself.
 /// assert!(ifelse!(false), false);
 /// ```
+#[macro_export]
 macro_rules! ifelse {
-    ($condition:expr , $true_expr:expr , $($opt:tt)*) => {
-        if $condition {
+    ($cond:expr , $true_expr:expr , $($arg:tt)*) => {
+        if $cond {
             $true_expr
         } else {
-            ifelse!($($opt)*)
+            ifelse!($($arg)*)
         }
     };
     ($false_expr:expr) => ($false_expr);
@@ -49,5 +49,4 @@ mod tests {
         assert_eq!(ifelse!(1 < 0, true, 1 < 0, true, false), false);
         assert_eq!(ifelse!(false), false);
     }
-
 }
